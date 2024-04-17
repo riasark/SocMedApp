d95864d29f2216adc4f78f8e836ad76691a56d08
ria sarkar <jitasark@gmail.com>
2024-04-16 22:26:18 -0400

    added more express functionality

--- server/server.js
+++ server/server.js
@@ -2,4 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const dotenv = reqiure('dotenv');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
