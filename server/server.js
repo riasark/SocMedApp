4e2ab49695d8553a1d99313f86bc7859031f7f40
mjaramillo15 <mjaramillo@umass.edu>
2024-04-18 18:26:29 -0400

    mongoDB connection

--- server/server.js
+++ server/server.js
@@ -4,7 +4,16 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+dotenv.config()
+
 const app = express();
 app.use(json());
 app.use(cors());
-const port = 3030;
+
+const port = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(port, () => console.log(`Server Port: ${port}`));
+}).catch((error) => console.log(`${error} did not connect`));
