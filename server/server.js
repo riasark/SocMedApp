cae000f432bdb5a572eeb84cf0afcd3d6f6b8d4c
ria sarkar <jitasark@gmail.com>
2024-04-18 18:32:20 -0400

    changed port structure

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-const port = process.env.PORT || 3030;
+const port = 3030;
 mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(port, () => console.log(`Server Port: ${port}`));
 }).catch((error) => console.log(`${error} did not connect`));
