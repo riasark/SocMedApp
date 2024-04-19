21950133226f4d913360b450f8d28702bb8d2b24
ShiraBen-Anat <sbenanat@umass.edu>
2024-04-19 11:19:57 -0400

    simple login page frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+
+import "preline/preline";
+import { IStaticMethods } from "preline/preline";
+// window.HSStaticMethods = {
+//   // Define the methods or properties of the interface
+//   // For example:
+//   someMethod: function() {
+//     // Implementation of the method
+//   },
+//   someProperty: 'some value'
+// };
 
 function App() {
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   window.HSStaticMethods.autoInit();
+  // }, [location.pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
