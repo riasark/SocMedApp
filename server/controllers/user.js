4f25f6ef92b7b4b816ef3ab1138291a4e46b0950
ria sarkar <jitasark@gmail.com>
2024-04-20 15:07:58 -0400

    added sidebar component that populates with a user's hobbies

---
+++ server/controllers/user.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js";
+import Hobby from "../models/Hobby.js"
+
+export const userHobbies = async (req, res) => {
+    try {
+        const { user } = req.params;
+        const userInfo = await User.findById(user);
+        const hobbies = userInfo.hobbies;
+        const toReturn = [];
+        for (const hobbyId of hobbies) { 
+            const hob = await Hobby.findById(hobbyId); 
+            toReturn.push(hob.name);
+        }
+        res.json(toReturn);
+    } catch(err) { 
+        res.json({ message: err.message });
+    }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { userFeed, userHobbyFeed } from "../controllers/post.js";
+import { userHobbies } from "../controllers/user.js"
 
 const routes = express.Router()
 
 routes.get("/:author/feed", userFeed);
 routes.get("/:author/hobbyfeed", userHobbyFeed);
+routes.get("/:user", userHobbies);
 
 export default routes;
