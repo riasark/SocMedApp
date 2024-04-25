9cbb11d8e5deec23f722e93bb11231800ecbc518
ShiraBen-Anat <78280400+ShiraBen-Anat@users.noreply.github.com>
2024-04-25 12:16:27 -0400

    Revert "Merging userprof into profile to integrate backend into frontend"

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,22 +1,17 @@
 import User from "../models/User.js";
 import Hobby from "../models/Hobby.js"
 
-export const userprof = async (req, res) => {
-    try {
-        const { user } = req.params;
-        const toReturn = [];
-        const userInfo = await User.findById(user);
-        const userPosts = aait 
-    }catch(err){
-        res.json({message: err.message});
-    }
-}
-
 export const userHobbies = async (req, res) => {
     try {
         const { user } = req.params;
         const userInfo = await User.findById(user);
-        res.json(userInfo);
+        const hobbies = userInfo.hobbies;
+        const toReturn = [];
+        for (const hobbyId of hobbies) { 
+            const hob = await Hobby.findById(hobbyId); 
+            toReturn.push(hob.name);
+        }
+        res.json(toReturn);
     } catch(err) { 
         res.json({ message: err.message });
     }
