87f78b3b46ddd7638599b66d4e45daee7b7ad65e
ria sarkar <jitasark@gmail.com>
2024-04-26 12:55:30 -0400

    OKAYYY route running, now i have to get the hobbypage to go there

---
+++ server/controllers/hobbies.js
@@ -0,0 +1,23 @@
+import User from "../models/User.js";
+import Hobby from "../models/Hobby.js"
+
+export const joinHobby = async (req, res) => {
+    try {
+        const { author, hobby } = req.params;
+        const user = User.findById(author);
+        user.hobbies.push(hobby);
+        res.json("Success"); 
+    }catch(err){
+        res.json({message: err.message});
+    }
+}
+
+export const hobbyId = async (req, res) => {
+    try {
+        const { hobbyName } = req.body;
+        const hobbyId = await Hobby.findOne({name: hobbyName});
+        res.json(hobbyId._id);
+    }catch(err){
+        res.json({message: err.message});
+    }
+}
--- server/routes/hobbyRoutes.js
+++ server/routes/hobbyRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { hobbyFeed } from "../controllers/post.js";
 import { joinHobby } from "../controllers/user.js";
+import { hobbyId } from "../controllers/hobbies.js";
 
 const routes = express.Router();
 
 routes.get("/:hobby", hobbyFeed);
 routes.patch("/:author/:hobby", joinHobby);
+routes.post("/getId", hobbyId);
 
 export default routes;
