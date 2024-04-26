31588e36f14331353042e1c99cb74141c66cbc32
ShiraBen-Anat <sbenanat@umass.edu>
2024-04-26 10:14:37 -0400

    create post now populated from backend needs to send info back

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,4 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+
 const CreatePost = ({ onCloseModal }) => {
+
+    const location = useLocation()
+  const [hobbies, setHobbies] = useState([]);
+  const [hid, setHid] = useState('');
+  const [content, setContent] = useState('');
+
+    const submitPost = () => {
+        console.log(`hid: ${hid}`);
+        console.log(`content: ${content}`);
+        // axios call to send info to backend
+        onCloseModal();
+    }
+
+  useEffect(() => {
+    const fetchUserHobbies = async () => {
+      try {
+        const queryParams = new URLSearchParams(location.search);
+        const userId = queryParams.get('userId');
+        const response = await axios.get(`http://localhost:3030/users/${userId}`); 
+        if(Array.isArray(response.data)){
+          setHobbies(response.data);
+          console.log(`hobbies: ${JSON.stringify(response.data)}`)
+        }  
+        else{
+          console.log(response.data)
+        }
+      } catch (error) {
+        console.error("Error fetching user hobbies:", error);
+      }
+    };
+
+    fetchUserHobbies();
+  }, [location.search]);
+
     return (
         <div id="hs-notifications" class={`hs-overlay size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto`}>
           <div class="mt-[100px] hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0  ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
@@ -22,7 +60,12 @@ const CreatePost = ({ onCloseModal }) => {
 
                 <div class="space-y-4">
                   <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-                  
+                    <select  onChange={(e) => setHid(e.target.value)} class="py-3 px-4 pe-9 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
+                        <option selected="">Choose a Hobby Community to Post to</option>
+                        {hobbies.map((hobby) => (
+                            <option value={hobby._id}>{hobby}</option>
+                        ))}
+                    </select>
                   </div> 
 
                   {/* <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
@@ -30,7 +73,7 @@ const CreatePost = ({ onCloseModal }) => {
                   </div> */}
 
                   <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
-                  <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                  <textarea onChange={(e) => setContent(e.target.value)} id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 
                   </div>
                 </div>
@@ -40,9 +83,9 @@ const CreatePost = ({ onCloseModal }) => {
                 {/* <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-800 dark:text-white dark:hover:bg-neutral-800" data-hs-overlay="#hs-notifications">
                   Cancel
                 </button> */}
-                <a class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
+                <button onClick={submitPost} class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
                     Create Post
-                </a>
+                </button>
               </div>
             </div>
           </div>
