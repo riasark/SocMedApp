7f4a1556b5f08855b2c78e355129a40f4dfca9fc
ria sarkar <jitasark@gmail.com>
2024-04-27 14:42:30 -0400

    drop down populated

--- client/src/components/DropDown.js
+++ client/src/components/DropDown.js
@@ -2,15 +2,16 @@ import React from "react";
 
 function DropDown() {
 
+    const hobbies = ["Art", "Gardening", "Stem", "Rock Climbing", "Music", "Movies", "Sports"];
+
     return (
         <form class="mt-2 max-w-lg mx-auto">
             <label for="communities" class="block mb-2 text-sm font-medium text-gray-900 dark:text-neutral-700">Select an option</label>
             <select id="communities" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option select>Choose your communities</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
+                <option select>Choose a hobby to start!</option>
+                {hobbies.map((hobby) => {
+                     return <option value={hobby}>{hobby}</option>
+                })}
             </select>
         </form>
         // <div class="hs-dropdown relative inline-flex">
