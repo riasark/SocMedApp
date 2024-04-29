3a44e194e3a9328d5a125672500ccaa2a1f1a9fa
ShiraBen-Anat <sbenanat@umass.edu>
2024-04-29 10:30:08 -0400

    delete button ui done

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -17,19 +17,33 @@ function Post(props) {
         const findCurrUser = async () => {
             const info = await axios.get(`http://localhost:3030/users/${userId}/specific`);
             setAccountUser("@" + info.data.username);
-            console.log(account, props.username)
+            // console.log(account, props.username)
         }
         findCurrUser();
-    })
+    });
 
     const isCurrentUser = account === props.username;
 
     const date = new Date(props.time).toLocaleString();
     // date.toDateString();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const deletePost = () => {
+        // delete post logic send to backend
+    }
+
     return (
     <div>
-        <a class="group flex flex-col bg-white border rounded-xl transition dark:bg-neutral-900 dark:border-neutral-800" href="/">
+        <div class="group flex flex-col bg-white border rounded-xl transition dark:bg-neutral-900 dark:border-neutral-800">
         <div class="p-4 md:p-5 flex flex-wrap">
             <div class="pb-5 w-[80px]">
                 <div class="relative inline-block">
@@ -67,23 +81,17 @@ function Post(props) {
                 <h1>{date}</h1>
             </div>
             <div>
-            {isCurrentUser && (
-                 <div class="hs-dropdown relative inline-flex">
-                    <button id="hs-dropdown-custom-icon-trigger" type="button" class="hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                        <svg class="flex-none size-4 text-gray-600 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
-                    </button>
-                        
-                    <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700" aria-labelledby="hs-dropdown-custom-icon-trigger">
-                        <button class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700">
-                        Newsletter
-                        </button>
-                    </div>
+            <div class="relative inline-flex">
+                    <button type="button" onClick={openModal} class={`${isCurrentUser ? "" : "disabled opacity-0"} hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold rounded-lg border border-gray-200 bg-blue-600 hover:bg-blue-700 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800`} >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button> 
                 </div>
-            )}
             </div>
         </div>
        
-        </a>
+        </div>
         {props.comments.map((comment) => (
             <div>
             <Comment comment={comment}/>
@@ -91,6 +99,37 @@ function Post(props) {
             </div>
         ))}
         <WriteComment pid={props.id}/>
+        <div id="hs-slide-down-animation-modal" class={`hs-overlay ${isModalOpen ? '' : "hidden"} mt-4 size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none`}>
+                    <div class={`hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 ${isModalOpen ? "" : "opacity-0"} ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto`}>
+                      <div class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                        <div class="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
+                          <h3 class="font-bold text-gray-800 dark:text-white">
+                            Delete Post
+                          </h3>
+                          <button type="button" onClick={closeModal} class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" data-hs-overlay="#hs-slide-down-animation-modal">
+                            <span class="sr-only">Close</span>
+                            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                              <path d="M18 6 6 18"></path>
+                              <path d="m6 6 12 12"></path>
+                            </svg>
+                          </button>
+                        </div>
+                        <div class="p-4 overflow-y-auto">
+                          <p class="mt-1 text-gray-800 dark:text-neutral-400">
+                            Are you sure you want to delete this post? This action can not be reverted.
+                          </p>
+                        </div>
+                        <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
+                          <button type="button" onClick={closeModal} class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" data-hs-overlay="#hs-slide-down-animation-modal">
+                            Cancel
+                          </button>
+                          <button type="button" onClick={deletePost} class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                            Delete Post
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
         </div>
     )
 }
