17144d9962828d909887e5af9a1a13c5d33b6a1f
ria sarkar <jitasark@gmail.com>
2024-04-28 22:22:09 -0400

    logout button added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -137,6 +137,16 @@ const Header = ({ onOpenModal }) => {
             </svg>
           </button>
         </a>
+
+        <a href={`http://localhost:3000/login`}>
+          <button type="button" class="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" data-hs-offcanvas="#hs-offcanvas-right">
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/>
+          </svg>
+          </button>
+        </a>
+
+        
         
         <div class="hs-dropdown relative inline-flex [--placement:bottom-right]">
           <a href={`http://localhost:3000/profile?userId=${userId}`}>
