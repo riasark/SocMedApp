32882c3740d403f71ef58e534f3e79c255fb1ea5
mjaramillo15 <mjaramillo@umass.edu>
2024-05-08 18:38:46 -0400

    background gradient

--- client/src/pages/CreateProfilePage.js
+++ client/src/pages/CreateProfilePage.js
@@ -13,13 +13,13 @@ function CreateProfilePage() {
     const [password, setPassword] = useState('');
     const pfps = ['annie', 'boat', 'britta', 'camera', 'coolgirl', 'coolguy', 'man', 'troy', 'woman'];
     const hobbies = [
-        "Art", 
-        "Gardening", 
-        "Stem", 
-        "Rock Climbing", 
-        "Music", 
-        "Movies", 
-        "Sports", 
+        "Art",
+        "Gardening",
+        "Stem",
+        "Rock Climbing",
+        "Music",
+        "Movies",
+        "Sports",
         "Astronomy",
         "Birdwatching",
         "Cooking",
@@ -59,84 +59,90 @@ function CreateProfilePage() {
     }
 
     return (
-        <div className="max-w-[85rem] px-4 py-2 sm:px-6 lg:px-8 lg:py-10 mx-auto">
-            <div className="max-w-xl mx-auto">
-                <div className="text-center">
-                    <h1 className="text-3xl font-bold text-gray-800 sm:text-3xl dark:text-neutral-800">
-                        Create Your User Profile
-                    </h1>
-                    <p className="mt-1 text-gray-600 dark:text-neutral-600">
-                        Join An Endless Amount of Hobbies!
-                    </p>
-                </div>
+        <div>
+            <div aria-hidden="true" class="flex absolute -top-60 start-1/2 transform -translate-x-1/2">
+                <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-full h-[44rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-pink-600/50 dark:to-pink-600"></div>
+                <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-lavender-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem] dark:from-lavender-900/70 dark:via-pink-700/60 dark:to-pink-600/70"></div>
             </div>
+            <div className="max-w-[85rem] px-4 py-2 sm:px-6 lg:px-8 lg:py-10 mx-auto relative z-10">
+                <div className="max-w-xl mx-auto">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-bold text-gray-800 sm:text-3xl dark:text-neutral-800">
+                            Create Your User Profile
+                        </h1>
+                        <p className="mt-1 text-gray-600 dark:text-neutral-600">
+                            Join An Endless Amount of Hobbies!
+                        </p>
+                    </div>
+                </div>
 
-            <div className="mt-5 max-w-lg mx-auto bg-pink-300 border-pink-300 rounded-xl shadow-sm dark:bg-pink-300 dark:border-pink-300">
-                {/* <!-- Card --> */}
-                <div className="flex flex-col border rounded-xl p-4 sm:p-4 lg:p-8 dark:border-neutral-700">
-                    <h2 className="mb-8 text-xl text-center font-semibold text-pink-600 dark:text-neutral-600">
-                        Fill With Your Credentials
-                    </h2>
+                <div className="mt-5 max-w-lg mx-auto bg-pink-300 border-pink-300 rounded-xl shadow-sm dark:bg-pink-300 dark:border-pink-300">
+                    {/* <!-- Card --> */}
+                    <div className="flex flex-col border rounded-xl p-4 sm:p-4 lg:p-8 dark:border-neutral-700">
+                        <h2 className="mb-8 text-xl text-center font-semibold text-pink-600 dark:text-neutral-600">
+                            Fill With Your Credentials
+                        </h2>
 
-                    <form>
-                        <div className="grid gap-2 lg:gap-2">
-                            {/* <!-- Grid --> */}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
-                                <div>
-                                    <label className="block text-sm mb-2 text-white">First Name</label>
-                                    <input type="text" onChange={(e) => setFirstName(e.target.value)} name="hs-firstname-contacts-1" id="hs-firstname-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                        <form>
+                            <div className="grid gap-2 lg:gap-2">
+                                {/* <!-- Grid --> */}
+                                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+                                    <div>
+                                        <label className="block text-sm mb-2 text-white">First Name</label>
+                                        <input type="text" onChange={(e) => setFirstName(e.target.value)} name="hs-firstname-contacts-1" id="hs-firstname-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                                    </div>
+
+                                    <div>
+                                        <label className="block mb-2 text-sm text-white">Last Name</label>
+                                        <input type="text" onChange={(e) => setLastName(e.target.value)} name="hs-lastname-contacts-1" id="hs-lastname-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                                    </div>
                                 </div>
+                                {/* <!-- End Grid --> */}
+
+                                {/* <!-- Grid --> */}
+                                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+                                    <div>
+                                        <label className="block mb-2 text-sm font text-white">Username</label>
+                                        <input type="text" onChange={(e) => setUserName(e.target.value)} name="hs-email-contacts-1" id="hs-email-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                                    </div>
+
+                                    <div>
+                                        <label className="block mb-2 text-sm font text-white">Password</label>
+                                        <input type="text" onChange={(e) => setPassword(e.target.value)} name="hs-phone-number-1" id="hs-phone-number-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                                    </div>
+                                </div>
+                                {/* <!-- End Grid --> */}
 
                                 <div>
-                                    <label className="block mb-2 text-sm text-white">Last Name</label>
-                                    <input type="text" onChange={(e) => setLastName(e.target.value)} name="hs-lastname-contacts-1" id="hs-lastname-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
+                                    <label className="block mb-2 text-sm font text-white">Select a hobby</label>
+                                    <select onChange={(e) => setChosen(e.target.value)} id="communities" className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option>Choose a hobby to start!</option>
+                                        {hobbies.map((hobby, index) => {
+                                            return <option key={hid[index]} value={hid[index]}>{hobby}</option>
+                                        })}
+                                    </select>
+                                </div>
+                                <div>
+                                    <label className="block mb-2 text-sm font text-white">Select a profile picture</label>
+                                    <select onChange={(e) => setProfilePic(e.target.value)} id="profile-pic" className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option>Choose a profile picture</option>
+                                        {pfps.map((pfp, index) => {
+                                            return <option key={index} value={pfp}>{pfp}</option>
+                                        })}
+                                    </select>
                                 </div>
                             </div>
                             {/* <!-- End Grid --> */}
 
-                            {/* <!-- Grid --> */}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
-                                <div>
-                                    <label className="block mb-2 text-sm font text-white">Username</label>
-                                    <input type="text" onChange={(e) => setUserName(e.target.value)} name="hs-email-contacts-1" id="hs-email-contacts-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
-                                </div>
-
-                                <div>
-                                    <label className="block mb-2 text-sm font text-white">Password</label>
-                                    <input type="text" onChange={(e) => setPassword(e.target.value)} name="hs-phone-number-1" id="hs-phone-number-1" className="py-3 px-4 block w-full border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral dark:border-neutral-700 dark:text-neutral-800 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" />
-                                </div>
+                            <div className="mt-6 grid">
+                                <button onClick={submitUser} type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font rounded-lg border border-transparent bg-pink-600 text-white hover:bg-pink-700 disabled:opacity-50 disabled:pointer-events-none">Submit Profile</button>
                             </div>
-                            {/* <!-- End Grid --> */}
+                        </form>
 
-                            <div>
-                                <label className="block mb-2 text-sm font text-white">Select a hobby</label>
-                                <select onChange={(e) => setChosen(e.target.value)} id="communities" className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option>Choose a hobby to start!</option>
-                                    {hobbies.map((hobby, index) => {
-                                        return <option key={hid[index]} value={hid[index]}>{hobby}</option>
-                                    })}
-                                </select>
-                            </div>
-                            <div>
-                                <label className="block mb-2 text-sm font text-white">Select a profile picture</label>
-                                <select onChange={(e) => setProfilePic(e.target.value)} id="profile-pic" className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option>Choose a profile picture</option>
-                                    {pfps.map((pfp, index) => {
-                                        return <option key={index} value={pfp}>{pfp}</option>
-                                    })}
-                                </select>
-                            </div>
-                        </div>
-                        {/* <!-- End Grid --> */}
-
-                        <div className="mt-6 grid">
-                            <button onClick={submitUser} type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font rounded-lg border border-transparent bg-pink-600 text-white hover:bg-pink-700 disabled:opacity-50 disabled:pointer-events-none">Submit Profile</button>
-                        </div>
-                    </form>
-
+                    </div>
                 </div>
+                {/* <!-- End Card --> */}
             </div>
-            {/* <!-- End Card --> */}
         </div>
     )
 }
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
         <div class="relative">
             {/* <!-- Gradients --> */}
             <div aria-hidden="true" class="flex absolute -top-60 start-1/2 transform -translate-x-1/2">
-                <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-full h-[44rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-pink-600/50 dark:to-pink-600"></div>
+                <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-full h-[44rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-pink-600 dark:to-pink-600"></div>
                 <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-lavender-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem] dark:from-lavender-900/70 dark:via-pink-700/60 dark:to-pink-600/70"></div>
             </div>
             {/* <!-- End Gradients --> */}
@@ -14,13 +14,13 @@ function LandingPage() {
             <div class="relative z-10">
                 <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-6 lg:py-16">
                     <div class="max-w-2xl text-center mx-auto my-20">
-                        <p class="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l from-pink-300 to-pink-600 text-transparent dark:from-pink-100 dark:to-pink-200">
+                        <p class="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l from-pink-600 to-pink-600 text-transparent dark:from-pink-300 dark:to-pink-600">
                             A New Way to Connect With People
                         </p>
 
                         {/* <!-- Title --> */}
                         <div class="mt-6 max-w-4xl">
-                            <h1 class="block font-serif text-white text-8xl md:text-8xl lg:text-9xl">
+                            <h1 class="block font-serif text-pink-300 text-8xl md:text-8xl lg:text-9xl">
                                 Hobbify
                             </h1>
                         </div>
