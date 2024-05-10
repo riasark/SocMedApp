e14e2abe9606d2c12548a34613aee743c576a0b7
Luke Waldow <lwaldow@umass.edu>
2024-05-10 16:15:42 -0400

    Fix like/delete buttons taking up width while disabled

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -115,15 +115,15 @@ function Post(props) {
           </div>
           <div>
             <div class="relative inline-flex">
-                    <button type="button" onClick={openModal} class={`${isCurrentUser ? "" : "disabled opacity-0"} hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold rounded-lg border border-gray-200 bg-red-600 hover:bg-red-700 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800`} >
+                    <button type="button" onClick={openModal} class={`${isCurrentUser ? "" : "disabled opacity-0 display-none"} hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold rounded-lg border border-gray-200 bg-red-600 hover:bg-red-700 text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800`} >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>
                     </button> 
                 </div>
               <div class="relative inline-flex">
-                    <p className={`${!isCurrentUser ? "" : "disabled opacity-0"} mr-2 mt-1.5`}>{props.likes}</p>
-                    <button type="button" onClick={toggleLike} class={`${!isCurrentUser ? "" : "disabled opacity-0"} rounded-full hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold border border-gray-200 ${liked ? "bg-pink-400 hover:bg-pink-500" : "bg-blue-600 hover:bg-blue-700"} text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800`} >
+                    <p className={`${!isCurrentUser ? "" : "disabled opacity-0 display-none"} mr-2 mt-1.5`}>{props.likes}</p>
+                    <button type="button" onClick={toggleLike} class={`${!isCurrentUser ? "" : "disabled opacity-0 display-none"} rounded-full hs-dropdown-toggle flex justify-center items-center size-9 text-sm font-semibold border border-gray-200 ${liked ? "bg-pink-400 hover:bg-pink-500" : "bg-blue-600 hover:bg-blue-700"} text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800`} >
                     <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <path d="M7 10v12"></path>
                       <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"></path>
