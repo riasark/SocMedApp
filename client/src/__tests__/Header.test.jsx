770d9d5c4fdd5536b3ea13a7d2c4e0b531931606
Claudia <czhu@umass.edu>
2024-05-12 22:50:03 -0400

    more changes

--- client/src/__tests__/Header.test.jsx
+++ client/src/__tests__/Header.test.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import Header from '../components/Header';
-import BrowserRouter, { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('Header component', () => {
   test('renders without crashing', () => {
-    render(<Header />);
+    render(<BrowserRouter><Header /></BrowserRouter>);
   });
 
   test('dropdown opens when input is clicked', () => {
     render(<BrowserRouter><Header /></BrowserRouter>);
     const input = screen.getByPlaceholderText('Type a name');
     fireEvent.click(input);
-    const dropdown = screen.getByTestId('dropdown');
+    const dropdown = screen.getByLabelText('drop-down');
     expect(dropdown).toBeInTheDocument();
   });
 
   test('dropdown closes when clicked outside', () => {
     render(<BrowserRouter><Header /></BrowserRouter>);
-    const input = screen.getByPlaceholderText('Type a name');
-    fireEvent.click(input);
-    const dropdown = screen.getByTestId('dropdown');
+    //const input = screen.getByPlaceholderText('Type a name');
+    fireEvent.mouseDown(document);
+    const dropdown = screen.getByLabelText('drop-down');
     expect(dropdown).toBeInTheDocument();
 
     fireEvent.click(document.body);
